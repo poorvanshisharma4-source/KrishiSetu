f2688a2b16954ff6834d53e311ffbc8afe5bd32c
krati <krati0457@gmail.com>
2026-07-28 21:33:43 +0530

    fix prerender suspense error on buyer messages

--- app/buyer/messages/page.tsx
+++ app/buyer/messages/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-
-import React, { useEffect, useMemo, useState } from 'react'
+export const dynamic = 'force-dynamic';
+import React, { useEffect, useMemo, useState, } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import {
   Search,
